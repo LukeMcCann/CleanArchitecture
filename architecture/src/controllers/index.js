aa4fddfab82dde523deac70e5f5b4e860545f485
Luke McCann <luke.mccann@connexone.co.uk>
2021-12-09 10:05:41 +0000

    Add response and user controller

---
+++ architecture/src/controllers/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const userControllers = require('./user');
+
+module.exports = {
+    userControllers,
+}
---
+++ architecture/src/controllers/user/addUser.controller.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const { Response } = require('../../frameworks/common/Response');
+
+module.exports = dependencies => {
+
+    const { useCases: addUserUseCase } = dependencies;
+
+    return addUser = async (req, res, next) => {
+        try {
+            const {
+                body = {}
+            } = req;
+
+            const {
+                id, 
+                name, 
+                lastName, 
+                gender, 
+                meta,
+            } = body;
+
+            const addUser = addUserUseCase(dependencies);
+            const response = await addUser.execute({
+                id, 
+                name, 
+                lastName, 
+                gender, 
+                meta,
+            });
+
+            res.json(new Response({
+                status: true, 
+                content: response,
+            }));
+            
+            next();
+
+        } catch(err) {
+            next(err);
+        }
+    }
+
+    return addUser;
+}
